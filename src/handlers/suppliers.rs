cd382c9471e6d79d209a9c95961f06bb27566e1e
keruhass <keruhass@gmail.com>
2026-03-10 18:26:08 +0000

    Added route to get all suppliers using the same bank by bank name

--- src/handlers/suppliers.rs
+++ src/handlers/suppliers.rs
@@ -22,7 +22,7 @@ pub async fn get_suppliers(
         Ok(Json(result))
     }
 }
-
+// 1. GET /api/suppliers/by-inn/inn
 pub async fn get_supplier_by_inn(
     State(state): State<Arc<AppState>>,
     Path(inn_id): Path<String>,
@@ -53,5 +53,34 @@ pub async fn get_supplier_by_inn(
             StatusCode::NOT_FOUND,
             format!("Поставщик с ИНН {} не был найден.", inn_id),
         )),
+    }
+}
+
+pub async fn get_suppliers_by_bank(
+    State(state): State<Arc<AppState>>,
+    Path(bank_id): Path<String>,
+) -> Result<Json<Vec<SupplierName>>, (StatusCode, String)> {
+    let result = sqlx::query_as!(
+        SupplierName,
+        "
+        SELECT
+            s.supplier_name
+        FROM suppliers s
+        JOIN banks b ON s.bank_id = b.bank_id 
+        WHERE b.bank_name = $1
+        ",
+        bank_id
+    )
+    .fetch_all(&state.db)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    if result.is_empty() {
+        Err((
+            StatusCode::NOT_FOUND,
+            format!("Поставщики с банком {} не были найдены.", bank_id),
+        ))
+    } else {
+        Ok(Json(result))
     }
 }
--- src/routes/suppliers.rs
+++ src/routes/suppliers.rs
@@ -3,11 +3,15 @@ use std::sync::Arc;
 use axum::routing::get;
 use axum::Router;
 
-use crate::handlers::suppliers::{get_supplier_by_inn, get_suppliers};
+use crate::handlers::suppliers::{get_supplier_by_inn, get_suppliers, get_suppliers_by_bank};
 use crate::state::AppState;
 
 pub fn router() -> Router<Arc<AppState>> {
     Router::new()
         .route("/suppliers/by-inn/{inn_id}", get(get_supplier_by_inn))
         .route("/suppliers", get(get_suppliers))
+        .route(
+            "/suppliers/by-bank-name/{bank_name}",
+            get(get_suppliers_by_bank),
+        )
 }
